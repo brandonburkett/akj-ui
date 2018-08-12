6bbdb61e1855002284b52796a67a94b7ff706227
Brandon Burkett <brandon@brandon-san.com>
2018-08-12 17:48:57 -0500

    update window mock in unit  test

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,6 +3,11 @@ import * as ReactDOM from 'react-dom';
 import App from './app';
 
 it('renders without crashing', () => {
+  // mocks
+  window.scrollTo = () => {
+    /*stub*/
+  };
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
