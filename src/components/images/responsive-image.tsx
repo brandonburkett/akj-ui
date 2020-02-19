f698263e425bc9edd00497d71dd039521bffc909
Brandon Burkett <brandon@brandon-san.com>
2020-02-19 12:44:38 -0600

    alt support

--- src/components/images/responsive-image.tsx
+++ src/components/images/responsive-image.tsx
@@ -7,7 +7,7 @@ import './responsive-image.css';
  * Width 100% images
  */
 const ResponsiveImage: StatelessComponent<ImgHTMLAttributes<HTMLImageElement>> = props => (
-  <img className="fit-img" {...props} />
+  <img className="fit-img" {...props} alt={props && props.alt ? props.alt : ''} />
 );
 
 export default ResponsiveImage;
