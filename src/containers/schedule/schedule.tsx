701f5bb1eb27ddee1faa58ae988dcbfadc309870
Brandon Burkett <brandon@brandon-san.com>
2021-03-21 12:17:22 -0500

    map updates

--- src/containers/schedule/schedule.tsx
+++ src/containers/schedule/schedule.tsx
@@ -279,7 +279,7 @@ const Schedule: React.FunctionComponent<IProps> = props => (
         <PanelContent>
           <ResponsiveMap>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109958.2234716137!2d-97.84696294558017!3d30.54380884107428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644d336df6c8b5d%3A0x5b4caa72f18797c9!2sAustin+Sports+Center!5e0!3m2!1sen!2sus!4v1535304224217"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110004.52931644666!2d-97.88896123812094!3d30.502893503723556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865b2da1155f378f%3A0x6bd60833d4e95385!2sCedar%20Park%20Dojo!5e0!3m2!1sen!2sus!4v1616347003684!5m2!1sen!2sus"
               width="800"
               height="600"
               style={{ border: 0 }}
