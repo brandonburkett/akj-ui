d1748eeaaac8d925b28338b8440577326932a19e
Brandon Burkett <brandon@brandon-san.com>
2022-11-11 11:02:26 -0600

    remove ga

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -89,20 +89,8 @@ class Head extends PureComponent<IProps> {
         <meta name="application-name" content="Austin Komei Jyuku" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <link rel="apple-touch-icon" href={kamonImage} />
-        {/* GTM */}
-        {env !== 'development' ? (
-          <script>
-            {`
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-              ga('create', '${gtmId || 'XXX-XXXXXXX'}', 'auto');
-              //ga('send', 'pageview');
-            `}
-          </script>
-        ) : null}
+        {/* TODO: analytics */}
+        {env !== 'development' ? null : null}
         {children}
       </Helmet>
     );
